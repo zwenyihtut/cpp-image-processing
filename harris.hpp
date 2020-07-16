53a4f4f87fb7fed55d7be8b87511cb794c2f89f8
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-07-16 08:37:56 +0630

    Harris: replace Matrix with Mat class

--- harris.hpp
+++ harris.hpp
@@ -1,46 +1,37 @@
 #pragma once
-#include <cmath>
 #include "canny.hpp"
+#include "mat.hpp"
 #include "neighbours.hpp"
 #include "pixels.hpp"
 #include "sobel.hpp"
 #include "utility.hpp"
+#include <cmath>
 
-template <long unsigned ROW, long unsigned COL>
-Matrix<ROW, COL>& operator+=(Matrix<ROW, COL>& a, const Matrix<ROW, COL>& b) {
-  for (unsigned y = 0; y < ROW; ++y) {
-    for (unsigned x = 0; x < COL; ++x) {
-      a[y][x] += b[y][x];
-    }
-  }
-  return a;
-}
-
-Image<PixelG8> harris(Image<PixelG8>& img) {
+Image<PixelG8> harris(Image<PixelG8> &img) {
   const auto [xIntensities, yIntensities] = sobelGetXandYIntensities(img);
 
   const auto WIDTH = img.width();
   const auto HEIGHT = img.height();
   Image<PixelRGB8> anotherOne = mapImage<Image<PixelG8>, Image<PixelRGB8>>(
       img,
-      [](const PixelG8& pix) -> typename Image<PixelRGB8>::pixel_t::value_t {
+      [](const PixelG8 &pix) -> typename Image<PixelRGB8>::pixel_t::value_t {
         const auto [v] = pix.val();
-        return { v, v, v };
+        return {v, v, v};
       });
+
+  Mat<double> sum({2, 2}, {0, 0, 0, 0});
 
   Image<PixelG8> newImg(img.width(), img.height());
   for (unsigned y = 2; y < HEIGHT - 2; ++y) {
     for (unsigned x = 2; x < WIDTH - 2; ++x) {
-      Matrix<2, 2> sum = { { { 0, 0 }, { 0, 0 } } };
+      sum = {0, 0, 0, 0};
 
       for (unsigned dy = y - 2; dy < y + 2; ++dy) {
         for (unsigned dx = x - 2; dx < x + 2; ++dx) {
           const auto ix = xIntensities[dy][dx].val();
           const auto iy = yIntensities[dy][dx].val();
 
-          Matrix<2, 2> curr = { { { ix * ix, ix * iy },
-                                  { ix * iy, iy * iy } } };
-          sum += curr;
+          sum += {ix * ix, ix * iy, ix * iy, iy * iy};
         }
       }
 
@@ -61,8 +52,8 @@ Image<PixelG8> harris(Image<PixelG8>& img) {
       const double R = det - (0.04 * trace * trace);
 
       if (R > 10e7) {
-        newImg[y][x].val({ 255 });
-        anotherOne[y][x].val({ 255, 0, 0 });
+        newImg[y][x].val({255});
+        anotherOne[y][x].val({255, 0, 0});
       }
     }
   }
