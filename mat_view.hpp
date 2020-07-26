ea9a54c70edccc1d82be3062a6037e58fd624023
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-07-27 01:50:21 +0630

    MatView: add mat_view.hpp
    Extract MatView classes from mat.hpp into a separate file mat_view.hpp

---
+++ mat_view.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include "mat.hpp"
+
+template <typename E, typename M, typename D>
+class MatViewBase {
+ public:
+  MatViewBase(M& matrix,
+              unsigned currentDimension,
+              unsigned index,
+              unsigned offset)
+      : mMatrix(matrix),
+        mCurrentDimension(currentDimension),
+        mIndex(index),
+        mOffset(offset) {}
+
+  D operator[](unsigned index);
+  const D operator[](unsigned index) const;
+  operator E() const;
+
+ protected:
+  void assertRange(unsigned index) const;
+
+  M& mMatrix;
+  unsigned mCurrentDimension;
+  unsigned mIndex;
+  unsigned mOffset;
+};
+
+template <typename E>
+class MatView : public MatViewBase<E, Mat<E>, MatView<E>> {
+ public:
+  using MatViewBase<E, Mat<E>, MatView<E>>::MatViewBase;
+  MatView& operator=(const E& value);
+
+};
+
+template <typename E>
+class ConstMatView : public MatViewBase<E, const Mat<E>, ConstMatView<E>> {
+ public:
+  using MatViewBase<E, const Mat<E>, ConstMatView<E>>::MatViewBase;
+};
+
+template <typename E, typename M, typename D>
+D MatViewBase<E, M, D>::operator[](unsigned index) {
+  assertRange(index);
+
+  unsigned stride = this->mMatrix.mOffsetMultipliers[this->mCurrentDimension];
+  return D(this->mMatrix, this->mCurrentDimension + 1, index,
+                    this->mOffset + this->mIndex * stride);
+};
+
+template <typename E, typename M, typename D>
+const D MatViewBase<E, M, D>::operator[](unsigned index) const {
+  assertRange(index);
+
+  unsigned stride = this->mMatrix.mOffsetMultipliers[this->mCurrentDimension];
+  return D(this->mMatrix, this->mCurrentDimension + 1, index,
+                    this->mOffset + this->mIndex * stride);
+};
+
+template <typename E, typename M, typename D>
+MatViewBase<E, M, D>::operator E() const {
+  assert(this->mCurrentDimension == this->mMatrix.mDimensions.size() - 1);
+  return this->mMatrix.mElements.at(this->mOffset + this->mIndex);
+}
+
+template <typename E>
+MatView<E>& MatView<E>::operator=(const E& value) {
+  assert(this->mCurrentDimension == 0);
+  const auto i = this->mOffset + this->mIndex;
+  this->mMatrix.mElements.at(i) = value;
+  return *this;
+}
+
+template <typename E, typename M, typename D>
+void MatViewBase<E, M, D>::assertRange(unsigned index) const {
+  if (index >= this->mMatrix.dimension(this->mCurrentDimension + 1)) {
+    std::ostringstream errText;
+    errText << "Out of range access: index = " << index
+            << ", dimension length = "
+            << this->mMatrix.dimension(this->mCurrentDimension + 1)
+            << ", current dimension = " << this->mCurrentDimension;
+    throw std::out_of_range(errText.str());
+  }
+}
+
+
