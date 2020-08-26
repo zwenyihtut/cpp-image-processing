d83b3058ff77abc5209a5ed8d585fbdf55e940ec
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-08-27 01:03:00 +0630

    run: group all `img::write` calls at the end of function

--- main.cpp
+++ main.cpp
@@ -21,15 +21,11 @@ extern "C" {
 
 void run(const Mat<uint8_t>& image) {
   std::cout << "Height: " << img::height(image) << '\n'
-    << "Width: " << img::width(image) << std::endl;
+            << "Width: " << img::width(image) << std::endl;
   auto grayscaledImage = grayscale(image);
-  img::write(grayscaledImage, "grayscale.png", PNG_COLOR_TYPE_GRAY);
   auto gauss = gaussian(grayscaledImage);
-  img::write(gauss, "gauss.png", PNG_COLOR_TYPE_GRAY);
-  img::write(sobel(gauss), "sobel.png", PNG_COLOR_TYPE_GRAY);
-  
-  img::write(canny(grayscaledImage, 50, 180), "canny.png", PNG_COLOR_TYPE_GRAY);
   auto corners = harris(grayscaledImage);
+
   Mat<uint8_t> harrisOutput = image;
   for (auto [x, y] : corners) {
     harrisOutput[y][x][0] = 255;
@@ -37,8 +33,15 @@ void run(const Mat<uint8_t>& image) {
     harrisOutput[y][x][2] = 0;
     harrisOutput[y][x][3] = 255;
   }
+
   img::write(harrisOutput, "harris.png");
   img::write(directionMap(gauss), "directions.png", PNG_COLOR_TYPE_RGB_ALPHA);
+  img::write(gauss, "gauss.png", PNG_COLOR_TYPE_GRAY);
+  img::write(sobel(gauss), "sobel.png", PNG_COLOR_TYPE_GRAY);
+  img::write(canny(grayscaledImage, 50, 180), "canny.png", PNG_COLOR_TYPE_GRAY);
+  img::write(gaussianXX(grayscaledImage), "gxx.png", PNG_COLOR_TYPE_GRAY);
+  img::write(grayscaledImage, "grayscale.png", PNG_COLOR_TYPE_GRAY);
+  
 }
 
 int main(int argc, char** argv) {
@@ -47,7 +50,7 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  auto image = img::read(argv[1]); 
+  auto image = img::read(argv[1]);
   run(image);
   return 0;
 }
