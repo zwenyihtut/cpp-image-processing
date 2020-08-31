6a9b720261e0f5f07c10613eea8b9689729ceed2
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-08-31 17:19:58 +0630

    Make changes to mat_main.cpp

--- mat_main.cpp
+++ mat_main.cpp
@@ -1,23 +1,33 @@
+#include <iomanip>
 #include <iostream>
+
+#include "box_filters.hpp"
+#include "gaussian.hpp"
 #include "integral_image.hpp"
-#include "mat_view_2d.hpp"
-#include "mat_view.hpp"
 
-int main() {
-  Mat<uint8_t> mat{ { 3, 4, 1 }, { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 } };
-  MatView2D<uint8_t> view(mat);
+constexpr auto SIZE = 21;
 
-  std::cout << (int)view[0][0] << std::endl;
-  std::cout << (int)view[0][1] << std::endl;
-  std::cout << (int)view[1][0] << std::endl;
-  std::cout << (int)view[1][1] << std::endl;
-  
-  IntegralImage<int> table(view);
-  
-  for (int y = 0; y < table.dimension(0); ++y) {
-    for (int x = 0; x < table.dimension(1); ++x) {
-      std::cout << table[y][x] << ' ';
+void makeMat(const BoxFilter& filter) {
+  Mat<int> mat({SIZE, SIZE});
+  for (const auto& box : filter) {
+    const auto [x0, y0, x1, y1] = box.coordinates();
+    for (int x = x0; x <= x1; ++x) {
+      for (int y = y0; y <= y1; ++y) {
+        mat[y][x] = box.val();
+      }
+    }
+  }
+
+  for (int y = 0; y < mat.dimension(0); ++y) {
+    for (int x = 0; x < mat.dimension(1); ++x) {
+      std::cout << std::setw(3) << mat[y][x] << ' ';
     }
     std::cout << '\n';
   }
+}
+
+int main() {
+  makeMat(makeGaussianBoxFilterXX(SIZE));
+  std::cout << "------------------------------------\n";
+  makeMat(makeGaussianBoxFilterXY(SIZE));
 }
