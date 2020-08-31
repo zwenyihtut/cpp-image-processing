404e9129cf5a67ce0e1b056b3d6e86290409c43a
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-08-31 17:19:24 +0630

    Add BoxFilters

---
+++ box_filters.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+
+
+class Box {
+ public:
+  struct Coord {
+    unsigned x0, y0, x1, y1;
+  };
+
+  Box(unsigned x0, unsigned y0, unsigned x1, unsigned y1, int8_t value)
+      : mCoord({x0, y0, x1, y1}), mVal(value) {}
+
+  Coord coordinates() const { return mCoord; }
+  int8_t val() const { return mVal; }
+
+  void transpose() {
+    std::swap(mCoord.x0, mCoord.y0);
+    std::swap(mCoord.x1, mCoord.y1);
+  }
+ private:
+  Coord mCoord;
+  int8_t mVal;
+};
+
+
+class BoxFilter {
+ public:
+  void addBox(const Box& box) { mBoxes.push_back(box); }
+
+  typename std::vector<Box>::iterator begin() { return mBoxes.begin(); }
+  typename std::vector<Box>::iterator end() { return mBoxes.end(); }
+
+  typename std::vector<Box>::const_iterator begin() const { return mBoxes.begin(); }
+  typename std::vector<Box>::const_iterator end() const { return mBoxes.end(); }
+
+  typename std::vector<Box>::const_iterator cbegin() const { return mBoxes.cbegin(); }
+  typename std::vector<Box>::const_iterator cend() const { return mBoxes.cend(); }
+
+  void transpose() {
+    for (auto& box : mBoxes) {
+      box.transpose();
+    }
+  }
+ private:
+  std::vector<Box> mBoxes;
+};
