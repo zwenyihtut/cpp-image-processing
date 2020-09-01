c31552cfd1a89a7f13c42bf26769c75294d9ff4c
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-09-01 11:17:34 +0630

    Move `normalize` function to header file
    This makes `normalize` usable from other files

--- gaussian.cpp
+++ gaussian.cpp
@@ -17,19 +17,6 @@ Mat<uint8_t> gaussianY(const Mat<uint8_t>& image) {
   return convolute(image, colKernal);
 }
 
-template <typename T>
-void normalize(Mat<T>& mat, T range_beg, T range_end) {
-  T min_val = *std::min_element(mat.begin(), mat.end());
-  T max_val = *std::max_element(mat.begin(), mat.end());
-
-  std::transform(mat.begin(), mat.end(), mat.begin(),
-                 [min_val, max_val, range_beg, range_end](T n) {
-                   return (range_end - range_beg) / (max_val - min_val) *
-                              (n - max_val) +
-                          range_beg;
-                 });
-}
-
 Mat<uint8_t> gaussianXX(const Mat<uint8_t>& image) {
   Mat<double> cpy = image.clone<double>();
   Mat<double> kernal({1, 7}, {0.09, 0.41, 0, -1.0, 0, 0.41, 0.09});
--- gaussian.hpp
+++ gaussian.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "mat.hpp"
+#include <iostream>
 
 Mat<uint8_t> gaussian(const Mat<uint8_t>& image);
 
@@ -17,3 +18,16 @@ class BoxFilter;
 BoxFilter makeGaussianBoxFilterXY(unsigned size);
 BoxFilter makeGaussianBoxFilterXX(unsigned size);
 BoxFilter makeGaussianBoxFilterYY(unsigned size);
+
+template <typename T>
+void normalize(Mat<T>& mat, T range_beg, T range_end) {
+  T min_val = *std::min_element(mat.begin(), mat.end());
+  T max_val = *std::max_element(mat.begin(), mat.end());
+
+  std::transform(mat.begin(), mat.end(), mat.begin(),
+                 [min_val, max_val, range_beg, range_end](T n) {
+                   return (range_end - range_beg) / (max_val - min_val) *
+                              (n - max_val) +
+                          range_beg;
+                 });
+}
