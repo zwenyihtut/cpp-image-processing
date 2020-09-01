82dc73c6921386777d986ceaf1ea510f46fdb951
Zwe Nyi Htut <zwenyihtut.kv@gmail.com>
2020-09-01 12:33:13 +0630

    IntegralImage: use vector to store data instead of Mat

--- integral_image.hpp
+++ integral_image.hpp
@@ -1,35 +1,40 @@
 #pragma once
-#include "mat.hpp"
+#include <vector>
+
 #include "mat_view_2d.hpp"
 
-template <typename T>
-class IntegralImage : public Mat<T> {
+class IntegralImage {
  public:
   template <typename E>
-  IntegralImage(const MatView2D<E>& mat) : Mat<T>({mat.height(), mat.width()}) {
+  IntegralImage(const MatView2D<E>& mat)
+      : mData(mat.height(), std::vector<int>(mat.width(), 0)) {
+
     for (unsigned y = 0; y < mat.height(); ++y) {
-      T sum = 0;
+      int sum = 0;
       for (unsigned x = 0; x < mat.width(); ++x) {
         sum += mat[y][x];
-        (*this)[y][x] = sum;
+        this->mData[y][x] = sum;
       }
     }
 
     for (unsigned x = 0; x < mat.width(); ++x) {
-      T sum = 0;
+      int sum = 0;
       for (unsigned y = 0; y < mat.height(); ++y) {
-        sum += (*this)[y][x];
-        (*this)[y][x] = sum;
+        sum += this->mData[y][x];
+        this->mData[y][x] = sum;
       }
     }
   }
 
-  T getSum(unsigned x0, unsigned y0, unsigned x1, unsigned y1) {
-    const T a = (x0 == 0 || y0 == 0) ? 0 : (*this)[y0 - 1][x0 - 1];
-    const T b = (y0 == 0 || x1 == 0) ? 0 : (*this)[y0 - 1][x1];
-    const T c = (y1 == 0 || x0 == 0) ? 0 : (*this)[y1][x0 - 1];
-    const T d = (y1 == 0 || x1 == 0) ? 0 : (*this)[y1][x1];
+  int getSum(unsigned x0, unsigned y0, unsigned x1, unsigned y1) const {
+    const int a = (x0 == 0 || y0 == 0) ? 0 : this->mData[y0 - 1][x0 - 1];
+    const int b = (y0 == 0 || x1 == 0) ? 0 : this->mData[y0 - 1][x1];
+    const int c = (y1 == 0 || x0 == 0) ? 0 : this->mData[y1][x0 - 1];
+    const int d = (y1 == 0 || x1 == 0) ? 0 : this->mData[y1][x1];
 
     return d + a - b - c;
   }
+
+ private:
+  std::vector<std::vector<int>> mData;
 };
